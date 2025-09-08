7251fbe80b510ebc1576d1e9e7984ff74558a04b
Cristian Tagle <cristian.gonzalez.gt@gmail.com>
2025-09-08 18:34:54 -0300

    fix(banner): restore abstract SVG and point BannerStrip to local asset

--- components/BannerStrip.tsx
+++ components/BannerStrip.tsx
@@ -1,22 +1,20 @@
-"use client";
-
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+"use client"
 
 export default function BannerStrip() {
-    return (
-        <div className="relative mx-auto max-w-6xl px-4">
-        <div className="relative h-[180px] overflow-hidden rounded-3xl shadow-soft md:h-[220px]">
-        {/* SVG escalable: se adapta a cualquier ancho */}
-        <Image
-        src="/images/banner-abstract.svg?v=1"
-        alt=""
-        role="presentation"
-        fill
-        priority
-        sizes="(max-width: 768px) 100vw, 960px"
-        className="object-cover"
+  return (
+    <div className="relative mx-auto max-w-6xl px-4">
+      <div className="relative h-[180px] overflow-hidden rounded-3xl shadow-soft md:h-[220px]">
+        {/* SVG local escalable y ligero */}
+        <img
+          src="/images/banner-abstract.svg"
+          alt=""
+          role="presentation"
+          className="absolute inset-0 h-full w-full object-cover"
+          loading="eager"
+          decoding="async"
         />
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  )
 }
